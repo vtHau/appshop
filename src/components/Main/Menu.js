1fe77eaf4eb7483d4f198feca55c53bd39bafd20
vtHau <crtrunghau@gmail.com>
2021-03-30 15:56:14 +0700

    add contact, search vv

--- src/components/Main/Menu.js
+++ src/components/Main/Menu.js
@@ -76,7 +76,7 @@ function Menu(props) {
     );
   }
 
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
 
   return (
     <View style={{flex: 1, backgroundColor: '#34b089'}}>
--- src/components/Main/Shop/Search/SearchView.js
+++ src/components/Main/Shop/Search/SearchView.js
@@ -1,29 +1,177 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  View,
+  Image,
+  Dimensions,
+} from 'react-native';
+
+import sp1 from '../../../../media/temp/sp3.jpeg';
+import sp4 from '../../../../media/temp/sp4.jpeg';
+
+function toTitleCase(str) {
+  return str.replace(
+    /\w\S*/g,
+    txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(),
+  );
+}
 
 function SearchView(props) {
   const {navigation} = props;
 
+  const gotoDetail = () => {
+    navigation.push('ProductDetail');
+  };
+
+  const {
+    product,
+    mainRight,
+    txtMaterial,
+    txtColor,
+    txtName,
+    txtPrice,
+    productImage,
+    txtShowDetail,
+    showDetailContainer,
+    wrapper,
+  } = styles;
+
   return (
-    <View style={styles.wrapper}>
-      <Text style={styles.text}>Search</Text>
-      <TouchableOpacity onPress={() => navigation.push('ProductDetail')}>
-        <Text>Go Details</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView style={wrapper}>
+      <View style={product}>
+        <Image source={sp1} style={productImage} />
+        <View style={mainRight}>
+          <Text style={txtName}>{toTitleCase('black dress')}</Text>
+          <Text style={txtPrice}>100$</Text>
+          <Text style={txtMaterial}>Material Fur</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={txtColor}>Color white</Text>
+            <View
+              style={{
+                height: 15,
+                width: 15,
+                backgroundColor: 'white',
+                borderRadius: 15,
+                marginLeft: 10,
+              }}
+            />
+          </View>
+          <TouchableOpacity style={showDetailContainer}>
+            <Text style={txtShowDetail}>SHOW DETAILS</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={product}>
+        <Image source={sp4} style={productImage} />
+        <View style={mainRight}>
+          <Text style={txtName}>{toTitleCase('black dress')}</Text>
+          <Text style={txtPrice}>100$</Text>
+          <Text style={txtMaterial}>Material Fur</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={txtColor}>Color white</Text>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={txtColor}>Color white</Text>
+              <View
+                style={{
+                  height: 15,
+                  width: 15,
+                  backgroundColor: 'white',
+                  borderRadius: 15,
+                  marginLeft: 10,
+                }}
+              />
+            </View>
+          </View>
+          <TouchableOpacity style={showDetailContainer}>
+            <Text style={txtShowDetail}>SHOW DETAILS</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
-export default SearchView;
+const {width} = Dimensions.get('window');
+const imageWidth = width / 4;
+const imageHeight = (imageWidth * 452) / 361;
 
 const styles = StyleSheet.create({
   wrapper: {
+    backgroundColor: '#F6F6F6',
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'yellow',
   },
-  text: {
-    fontSize: 40,
+  product: {
+    flexDirection: 'row',
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 2,
+    shadowColor: '#3B5458',
+    shadowOffset: {width: 0, height: 3},
+    shadowOpacity: 0.2,
+  },
+  productImage: {
+    width: imageWidth,
+    height: imageHeight,
+    flex: 1,
+    resizeMode: 'center',
+  },
+  mainRight: {
+    flex: 3,
+    justifyContent: 'space-between',
+  },
+  productController: {
+    flexDirection: 'row',
+  },
+  numberOfProduct: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  txtName: {
+    paddingLeft: 20,
+    color: '#A7A7A7',
+    fontSize: 20,
+    fontWeight: '400',
+    fontFamily: 'Avenir',
+  },
+  txtPrice: {
+    paddingLeft: 20,
+    color: '#C21C70',
+    fontSize: 15,
+    fontWeight: '400',
+    fontFamily: 'Avenir',
+  },
+  txtColor: {
+    paddingLeft: 20,
+    color: 'black',
+    fontSize: 15,
+    fontWeight: '400',
+    fontFamily: 'Avenir',
+  },
+  txtMaterial: {
+    paddingLeft: 20,
+    color: 'black',
+    fontSize: 15,
+    fontWeight: '400',
+    fontFamily: 'Avenir',
+  },
+  txtShowDetail: {
+    color: '#C21C70',
+    fontSize: 10,
+    fontWeight: '400',
+    fontFamily: 'Avenir',
+    textAlign: 'right',
+  },
+  showDetailContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    marginTop: 100,
   },
 });
+
+export default SearchView;
